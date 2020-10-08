85fbed2b0614783e673427a39cc2cb5fc22f1cf7
Nic <ignatenkolya@gmail.com>
2020-10-08 13:45:48 +0300

    Def commit

--- src/javapack/Parser.java
+++ src/javapack/Parser.java
@@ -11,36 +11,39 @@ public class Parser {
         boolean first_boolean = false;
         boolean second_boolean = false;
         StringBuilder complete_line = new StringBuilder();
-        while(!cvsText.isEmpty()) {
-            if(cvsText.startsWith(String.valueOf(Default_delimeter)) && !first_boolean && !second_boolean) {
+        while (!cvsText.isEmpty()) {
+            if (cvsText.startsWith(String.valueOf(Default_delimeter)) && !first_boolean && !second_boolean) {
                 complete_line.append(number_symb).append(separator2);
                 number_symb = 0;
             }
-            else if(cvsText.startsWith("\"") && number_symb == 0) {
-                int q = cvsText.indexOf("\"", 1);
-                if(q != -1) {first_boolean = true;}
-                else {number_symb++;}
-            }
-            else if(cvsText.startsWith("\"") && number_symb != 0) {
-                if(first_boolean) {
-                    first_boolean = false;
+            else if (cvsText.startsWith("\"") && number_symb == 0) {
+                int delimeter_comma = cvsText.indexOf("\"", 1);
+                if (delimeter_comma != -1) {
+                    first_boolean = true;
                 }
-            else if(cvsText.indexOf('/')==0 && cvsText.indexOf('*')==1) {
+                else {
+                    number_symb++;
+                }
+            }
+            else if (cvsText.startsWith("\"") && number_symb != 0) {
+                if (first_boolean) {
+                    first_boolean = false;
+                } else {
+                    number_symb++;
+                }
+            }
+            else if (cvsText.indexOf('/') == 0 && cvsText.indexOf('*') == 1) {
                 second_boolean = true;
             }
-            else if(cvsText.indexOf('*')==0 && cvsText.indexOf('/')==1) {
+            else if (cvsText.indexOf('*') == 0 && cvsText.indexOf('/') == 1) {
                 second_boolean = false;
                 cvsText = cvsText.substring(1);
             }
-            else {
-                number_symb++;
-                }
-            }
-            else if (!second_boolean){
+            else if (!second_boolean) {
                 number_symb++;
             }
-            cvsText = cvsText.substring(1);
-        }
+        cvsText = cvsText.substring(1);
+    }
         if(number_symb !=0) {
             complete_line.append(number_symb);
         }
