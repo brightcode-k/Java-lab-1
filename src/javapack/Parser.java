11221c9ce3a7efe61cbe96e4022d288ae0a5cbdc
Nic <ignatenkolya@gmail.com>
2020-10-09 12:15:42 +0300

    Def commit

--- src/javapack/Parser.java
+++ src/javapack/Parser.java
@@ -8,45 +8,49 @@ public class Parser {
 
     public static String LineParser(String cvsText, String separator) {
         int iterator = 0;
-        boolean first_boolean = false;
-        boolean second_boolean = false;
+        boolean first_boolean_quote = false;
+        boolean second_boolean_comma = false;
         StringBuilder complete_line = new StringBuilder();
-        while (!cvsText.isEmpty()) {
-            if (cvsText.startsWith(String.valueOf(Default_delimeter)) && !first_boolean && !second_boolean) {
+        while(!cvsText.isEmpty()) {
+            if(cvsText.startsWith(String.valueOf(Default_delimeter)) && !first_boolean_quote && !second_boolean_comma) {
                 complete_line.append(iterator).append(separator);
                 iterator = 0;
             }
-            else if (cvsText.startsWith("\"") && iterator == 0) {
-                int delimeter_comma = cvsText.indexOf("\"", 1);
-                if (delimeter_comma != -1) {
-                    first_boolean = true;
+
+            else if(cvsText.startsWith("\"") && iterator == 0) {
+                if(first_boolean_quote) {
+                    first_boolean_quote = false;
+                }
+                else {
+                    int q = cvsText.indexOf("\"", 1);
+                    if (q != -1) {
+                        first_boolean_quote = true;
+                    } else {
+                        iterator++;
+                    }
+                }
+            }
+            else if(cvsText.startsWith("\"") && iterator != 0) {
+                if(first_boolean_quote) {
+                    first_boolean_quote = false;
                 }
                 else {
                     iterator++;
                 }
             }
-            else if (cvsText.startsWith("\"") && iterator != 0) {
-                if (first_boolean) {
-                    first_boolean = false;
-                } else {
-                    iterator++;
-                }
+            else if(cvsText.indexOf('/')==0 && cvsText.indexOf('*')==1) {
+                second_boolean_comma = true;
             }
-            else if (cvsText.indexOf('/') == 0 && cvsText.indexOf('*') == 1) {
-                second_boolean = true;
+            else if(cvsText.indexOf('*')==0 && cvsText.indexOf('/')==1) {
+                second_boolean_comma = false;cvsText = cvsText.substring(1);
             }
-            else if (cvsText.indexOf('*') == 0 && cvsText.indexOf('/') == 1) {
-                second_boolean = false;
-                cvsText = cvsText.substring(1);
-            }
-            else if (!second_boolean) {
+            else if (!second_boolean_comma){
                 iterator++;
             }
-        cvsText = cvsText.substring(1);
-    }
-        if(iterator !=0) {
-            complete_line.append(iterator);
+            cvsText = cvsText.substring(1);
         }
+        if(iterator !=0) {
+            complete_line.append(iterator);}
         return complete_line.toString();
     }
 }
